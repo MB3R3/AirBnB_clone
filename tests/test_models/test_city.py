71dd70ecb3246d4eb1cc6486672439dfa7f95c6b
Mbereidem Essien <mbereidemessien@gmail.com>
2024-03-08 18:58:04 +0100

    feat: add file existence testcase for models

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,26 @@
+import unittest
+import pathlib as pl
+from os import getcwd
+
+class TestFileExistance(unittest.TestCase):
+    """
+    Base test case to check if file is present
+
+    ...
+
+    Methods
+    -------
+    assertIsFile(path)
+        Checks if a path exists, else raises an error
+
+    test()
+        Main test method that checks for intended file
+    """
+    def assertIsFile(self, path):
+        if not pl.Path(path).resolve().is_file():
+            raise AssertionError("File does not exist: %s" % str(path))
+
+    def test(self):
+        path = pl.Path(f"{getcwd()}/models/city.py")
+        self.assertIsFile(path)
+
