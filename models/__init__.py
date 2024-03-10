6806ab61b004f8bb3d8c98b2dd63bf0ee6c68306
Mbereidem Essien <mbereidemessien@gmail.com>
2024-03-10 07:06:35 +0100

    feat: run filestorsge enging in init dunder

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+To create a unique FileStorage instance for app
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()                                               storage.reload()
